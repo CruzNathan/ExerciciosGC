d0e9597c6c57f1a54b17ac60a3017deee0313626
CruzNathan <nan.nugget@hotmail.com>
2023-09-15 16:57:38 -0300

    resolucao exercicio 05 aula 01

--- aula01/src/exercicio05aula01.java
+++ aula01/src/exercicio05aula01.java
@@ -4,6 +4,12 @@
 
 public class exercicio05aula01 {
     public static void main(String[] args) {
-
+        int hora = 7;
+        int minuto = 15;
+        int segundo = 30;
+        int tempo = (((hora * 60) + minuto) * 60) + segundo;
+        System.out.print(tempo);
+        System.out.print("\n");
+        System.out.print(86400 - tempo);
     }
 }
