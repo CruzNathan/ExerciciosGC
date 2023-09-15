fc7adb92627cf6d8328db1d5a41897a4f91676b1
CruzNathan <nan.nugget@hotmail.com>
2023-09-15 17:48:01 -0300

    resolucao exercicio 07 aula 01

--- aula01/src/exercicio07aula01.java
+++ aula01/src/exercicio07aula01.java
@@ -2,8 +2,20 @@
 // o usuário a idade dele e o seu salário. A pessoa sópode comprar um automóvel se
 // for de maior e ganhar mais de R$ 2.000,00.
 
+import java.util.Scanner;
+
 public class exercicio07aula01 {
     public static void main(String[] args) {
+        System.out.print("Digite sua idade:");
+        Scanner inputIdade = new Scanner(System.in);
+        int idade = inputIdade.nextInt();
 
+        System.out.print("Digite seu salário");
+        Scanner inputSalario = new Scanner(System.in);
+        int salario = inputSalario.nextInt();
+
+        if (idade < 18 || salario < 2000){
+            System.out.print("Compra negada");
+        } else {System.out.print("Compra aprovada");}
     }
 }
