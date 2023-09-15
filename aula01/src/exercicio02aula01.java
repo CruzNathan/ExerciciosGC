6e1aed4997659ce3c3421be2208ad333f76fed3d
CruzNathan <nan.nugget@hotmail.com>
2023-09-15 15:40:44 -0300

    resolucao exercicio 02 aula 01

--- aula01/src/exercicio02aula01.java
+++ aula01/src/exercicio02aula01.java
@@ -2,6 +2,9 @@
 
 public class exercicio02aula01 {
     public static void main(String[] args) {
-
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
