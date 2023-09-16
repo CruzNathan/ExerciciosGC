d4c554e80e59b3f7d06fbd8f490b404dcbd81683
CruzNathan <nan.nugget@hotmail.com>
2023-09-16 15:40:08 -0300

    inicio da resolucao exercicio 11 aula 01

--- aula01/src/exercicio11aula01.java
+++ aula01/src/exercicio11aula01.java
@@ -1,8 +1,13 @@
 //Peça ao usuário que digite uma palavra ou frase e conte quantas vogais (a, e, i, o, u)
 // estão presentes. Exiba o número de vogais na tela.
 
+import java.util.Scanner;
+
 public class exercicio11aula01 {
     public static void main(String[] args) {
+        Scanner frase = new Scanner(System.in);
+        int numVogais = 0;
 
+       // frase = frase.toLowerCase();
     }
 }
