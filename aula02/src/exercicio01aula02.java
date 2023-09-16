35c16af38d485e52c15094cb3e1e37083c46fa36
CruzNathan <nan.nugget@hotmail.com>
2023-09-16 16:18:49 -0300

    resolucao exercicio 01 aula 02

--- aula02/src/exercicio01aula02.java
+++ aula02/src/exercicio01aula02.java
@@ -1,8 +1,31 @@
 //Crie um programa que determine e escreva os números primos compreendidos entre um
 // intervalo fornecido pelo usuário.
 
+import java.util.Scanner;
+
 public class exercicio01aula02 {
     public static void main(String[] args) {
+        System.out.print("Digite o início do inetrvalo: ");
+        Scanner inputIn = new Scanner(System.in);
+        int inicio = inputIn.nextInt();
+        System.out.print("Digite o fim do inetrvalo: ");
+        Scanner inputFim = new Scanner(System.in);
+        int fim = inputFim.nextInt();
 
+        if (inicio == 1) {inicio++;}
+
+
+        for (int i = inicio; i <= fim; i++) {
+            if( ehPrimo(i) )
+                System.out.println(i + " é primo.");
+        }
+    }
+
+    private static boolean ehPrimo(int numero) {
+        for (int j = 2; j < numero; j++) {
+            if (numero % j == 0)
+                return false;
+        }
+        return true;
     }
 }
