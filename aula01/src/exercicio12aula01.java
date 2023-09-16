422699bd9a6c8d05056987f9de9585d8043eb662
CruzNathan <nan.nugget@hotmail.com>
2023-09-16 15:38:42 -0300

    resolucao exercicio 12 aula 01

--- aula01/src/exercicio12aula01.java
+++ aula01/src/exercicio12aula01.java
@@ -2,8 +2,20 @@
 // euros ou reais. Peça ao usuário para inserir a quantia em dólares e a taxa
 // de câmbio atual. Em seguida, calcule e exiba o valor convertido.
 
+import java.util.*;
+
 public class exercicio12aula01 {
     public static void main(String[] args) {
+        System.out.print("Digite uma quantia em dólares (use ',' e não '.'): ");
+        Scanner inputDol = new Scanner(System.in);
+        float dolar = inputDol.nextFloat();
 
+        System.out.print("Digite a taxa de câmbio atual: ");
+        Scanner inputTaxa = new Scanner(System.in);
+        float taxa = inputTaxa.nextFloat();
+
+        float cambio = dolar * taxa;
+
+        System.out.print(cambio);
     }
 }
