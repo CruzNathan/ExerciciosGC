85b1a981baeacd5fc217f2a002b40bb683026554
CruzNathan <nan.nugget@hotmail.com>
2023-09-19 16:14:53 -0300

    inicio resolucao exercicio 01 aula 03

--- aula03/src/exercicio01aula03.java
+++ aula03/src/exercicio01aula03.java
@@ -1,5 +1,20 @@
 public class exercicio01aula03 {
-    public static void main(String[] args) {
 
+    private String nome;
+    private int cpf;
+    private int identConta;
+    private String banco;
+    private String endereco;
+    private double saldo;
+    private String horarioAtual;
+
+
+    public void saque(double valor){
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+        } else {
+            System.out.println("Erro: Saldo insuficiente")
+        }
     }
 }
+
