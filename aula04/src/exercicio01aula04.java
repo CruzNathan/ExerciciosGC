c59330548cbd636de70b1bacda22e93f067cbbbc
CruzNathan <nan.nugget@hotmail.com>
2023-09-19 15:41:04 -0300

    adicionando mains

--- aula04/src/exercicio01aula04.java
+++ aula04/src/exercicio01aula04.java
@@ -1,2 +1,5 @@
 public class exercicio01aula04 {
+    public static void main(String[] args) {
+
+    }
 }
--- aula04/src/exercicio02aula04.java
+++ aula04/src/exercicio02aula04.java
@@ -1,2 +1,5 @@
 public class exercicio02aula04 {
+    public static void main(String[] args) {
+
+    }
 }
