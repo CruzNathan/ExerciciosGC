c20ce983db973e99180cc6661d1318ce55d3ec49
CruzNathan <nan.nugget@hotmail.com>
2023-09-28 16:06:56 -0300

    inicio resolucao exercicio 03 aula 05

---
+++ aula05/src/exercicio03aula05/Tarefa.java
@@ -0,0 +1,13 @@
+package exercicio03aula05;
+
+public class Tarefa {
+    private String titulo;
+    private String data;
+    private String descricao;
+
+    public Tarefa(String titulo, String data, String descricao){
+        this.titulo = titulo;
+        this.data = data;
+        this.descricao = descricao;
+    }
+}
---
+++ aula05/src/exercicio03aula05/exercicio03aula05.java
@@ -0,0 +1,11 @@
+package exercicio03aula05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class exercicio03aula05 {
+    public static void main(String[] args) {
+        Tarefa primeira = new Tarefa("Limpar o quarto","29/09/2023", "Aspirar e passar o pano no quarto, abrir a janela e recolher roupas sujas");
+
+    }
+}
