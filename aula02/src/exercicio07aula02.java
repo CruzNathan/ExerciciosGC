9310f466f8736e8c61611e3d1dd1783c15b7171f
CruzNathan <nan.nugget@hotmail.com>
2023-10-19 14:10:42 -0300

    add: resolucao exercicio 07 aula 02

--- aula02/src/exercicio07aula02.java
+++ aula02/src/exercicio07aula02.java
@@ -4,8 +4,27 @@
 // do que 300.000, Leônidas deve dizer Um pouco mais!, caso o número dito pelo usuário
 // seja menor, ele dirá Um pouco menos!".
 
+import java.util.Scanner;
+
 public class exercicio07aula02 {
     public static void main(String[] args) {
+        System.out.println("Adivinhe quantos inimigos serão enfrentados! ");
+        Scanner scan = new Scanner(System.in);
 
+        while (true) {
+            int resposta = 300000;
+
+            System.out.println("\n Digite seu palpite: ");
+            int chute = scan.nextInt();
+
+            if (chute < resposta) {
+                System.out.println("Um pouco mais!");
+            } else if (chute > resposta) {
+                System.out.println("Um pouco menos!");
+            } else {
+                System.out.println("Acertou!");
+                break;
+            }
+        }
     }
 }
